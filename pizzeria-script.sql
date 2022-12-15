204a15660851f3a6c52a906e7867aba6aa05693e
RicardoLeonGuevara <110881039+RicardoLeonGuevara@users.noreply.github.com>
2022-12-14 19:33:59 -0500

    Add files via upload

---
+++ pizzeria-script.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO pizzeriadb.pizzeria VALUES (1,'Av Bolivar 468','DOMINO');
+INSERT INTO pizzeriadb.pizzeria VALUES (2,'Calle Lima 342','PIZZA HUT');
+INSERT INTO pizzeriadb.pizzeria VALUES (3,'Av Brasil 1234', 'PAPA JHON');
+INSERT INTO pizzeriadb.pizzeria VALUES (4,'Av Venezuela 677','PIZZA RAUL');
+
+SELECT * FROM pizzeriadb.pizzeria;
